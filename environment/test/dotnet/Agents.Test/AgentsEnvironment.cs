328e5dc2ae1c6a0f53f5411c10d213437c66b4b4
Branimir Angelov <55453003+branimirangelov@users.noreply.github.com>
2021-05-12 20:20:11 +0300

    feat: partial implementation of dotnet test env

--- environment/test/dotnet/Agents.Test/AgentsEnvironment.cs
+++ environment/test/dotnet/Agents.Test/AgentsEnvironment.cs
@@ -30,7 +30,7 @@ namespace Agents.Test
         public void Start(string agent, object message = null)
         {
             var target = Guid.NewGuid().ToString();
-            _references[target] = (agent, new[]{message.GetType()});
+            _references[target] = (agent, new[] { message.GetType() });
             Execute(target, message, nameof(AgentsEnvironment));
 
             RunMessagesLoop();
@@ -38,38 +38,58 @@ namespace Agents.Test
 
         private void RunMessagesLoop()
         {
-            while(_messages.Count > 0)
+            while (_messages.Count > 0)
             {
-                     
-            }            
+                var (sourceMessage, from) = _messages.Dequeue();
+                var sourceMessageType = sourceMessage.GetType();
+
+                var toRef = (string)sourceMessageType.GetProperty("To").GetValue(sourceMessage);
+                var (target, whitelist) = _references[toRef];
+                
+                var messageType = whitelist.FirstOrDefault(t => t.Name == sourceMessageType.Name);
+                if(messageType == null)
+                {
+                    throw new Exception($"Reference doesn't support message type {sourceMessageType.Name}");    
+                }
+
+                var message = CloneMessage(messageType, sourceMessage, from);
+                Execute(target, message, from);
+            }
         }
 
-        private void Execute(string target, object sourceMessage, string sourceAgent)
+        private void Execute(string target, object message, string from)
         {
             var (agent, types) = _references[target];
-            var targetType = types.First(t => t.Name == sourceMessage.GetType().Name);
-            var targetMessage = CloneMessage(targetType, sourceMessage, sourceAgent);            
-            
+            var targetType = types.First(t => t.Name == message.GetType().Name);
+            var targetMessage = CloneMessage(targetType, message, from);
+
             var (assmebly, state) = _agents[agent];
             var handler = assmebly.GetType($"{targetType.Name}Handler").GetMethod("Run");
-            var output = handler.Invoke(null, new object[]{targetMessage, sourceAgent, state});
+            var output = handler.Invoke(null, new object[] { targetMessage, from, state });
 
-            if(output is IEnumerable outputMessages)
+            if (output is IEnumerable outputMessages)
             {
                 foreach (var outputMessage in outputMessages)
                 {
-                    _messages.Enqueue(outputMessage);
-                } 
+                    _messages.Enqueue((outputMessage, target));
+                }
             }
             else
             {
-                _messages.Enqueue(output);
+                _messages.Enqueue((output, target));
             }
         }
 
         private object CloneMessage(Type targetType, object sourceMessage, string sourceAgent)
         {
-            throw new NotImplementedException();
+            var sourceMessageType = sourceMessage.GetType();
+            foreach(var property in sourceMessageType.GetProperties())
+            {
+                if(property.Name.EndsWith("Ref"))
+                {
+
+                }
+            }
         }
     }
 }
